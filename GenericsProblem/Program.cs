8a67d12132eb6789f953854a6961da46e4a5fdde
Ankit <ankitcool4370@gmail.com>
2022-11-26 22:23:15 +0530

    Welcome to the Generics

---
+++ GenericsProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Generics Problem : ");
+            FindMaximum.FindMaxValue(20, 40, 50);
+            Console.ReadLine();
+        }
+    }
+}
