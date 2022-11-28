9f9cd0290221e8066fb19b44f6eca23856ae396a
Ankit <ankitcool4370@gmail.com>
2022-11-28 22:05:44 +0530

    Adding all UC Case

--- GenericsProblem/GenericsProblem.cs
+++ GenericsProblem/GenericsProblem.cs
@@ -8,6 +8,40 @@ namespace GenericsProblem
 {
     public class FindMaximum
     {
+        public static void FindMaxValue(int first, int second, int third)
+        {
+            if (first.CompareTo(second) >= 0 && first.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("{0} is Maximum", first);
+            }
+            else if (second.CompareTo(first) >= 0 && second.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("{0} is  Maximum", second);
+            }
+            else if (third.CompareTo(first) >= 0 && third.CompareTo(second) >= 0)
+            {
+                Console.WriteLine("{0} is Maximum", third);
+            }
+            else
+                Console.WriteLine("All number are equal");
+        }
+        public static void FindMaxValue(float first, float second, float third)
+        {
+            if (first.CompareTo(second) >= 0 && first.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("{0} is Maximum", first);
+            }
+            else if (second.CompareTo(first) >= 0 && second.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("{0} is  Maximum", second);
+            }
+            else if (third.CompareTo(first) >= 0 && third.CompareTo(second) >= 0)
+            {
+                Console.WriteLine("{0} is Maximum", third);
+            }
+            else
+                Console.WriteLine("All number are equal");
+        }
         public static void FindMaxValue(string first, string second, string third)
         {
             if (first.CompareTo(second) >= 0 && first.CompareTo(third) >= 0)
@@ -25,6 +59,7 @@ namespace GenericsProblem
             else
                 Console.WriteLine("All number are equal");
         }
+
     }
 }
 
--- GenericsProblem/Program.cs
+++ GenericsProblem/Program.cs
@@ -10,10 +10,30 @@ namespace GenericsProblem
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Generics Problem : ");
             FindMaximum Number = new FindMaximum();
-            FindMaximum.FindMaxValue("zya", "zzz", "rew");
+            Console.WriteLine("Welcome to the Generics Problem : ");
+            Console.WriteLine("Please choose the option in below");
+            Console.WriteLine("1:- Find Maximum Number\n" + "2:- Find Maximum Using Generics");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FindMaximum.FindMaxValue(20, 40, 50);
+                    FindMaximum.FindMaxValue(20.5f, 10.3f, 11.3f);
+                    FindMaximum.FindMaxValue("zya", "zzz", "rew");
+                    break;
+
+                case 2:
+                    GenericsFindMax<int>.FindMaxValue(14, 23, 12);
+                    break;
+
+                default:
+                    Console.WriteLine("Please choose the number given range");
+                    break;
+            }
             Console.ReadLine();
-        }
+        } 
+        
     }
 }
