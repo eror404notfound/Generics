3e765be18a37d0bea254b5caaf25b9cb46aefb02
Ankit <ankitcool4370@gmail.com>
2022-11-29 15:15:28 +0530

    Performed In UC3 Refactor 2 Method

--- GenericsProblem/Program.cs
+++ GenericsProblem/Program.cs
@@ -13,7 +13,7 @@ namespace GenericsProblem
             FindMaximum Number = new FindMaximum();
             Console.WriteLine("Welcome to the Generics Problem : ");
             Console.WriteLine("Please choose the option in below");
-            Console.WriteLine("1:- Find Maximum Number\n" + "2:- Find Maximum Using Generics");
+            Console.WriteLine("1:- Find Maximum Number\n" + "2:- Find Maximum Using Generics\n" + "3:- Find max using Generics Class");
 
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -21,12 +21,21 @@ namespace GenericsProblem
                 case 1:
                     FindMaximum.FindMaxValue(20, 40, 50);
                     FindMaximum.FindMaxValue(20.5f, 10.3f, 11.3f);
-                    FindMaximum.FindMaxValue("zya", "zzz", "rew");
+                    FindMaximum.FindMaxValue("Apple", "Peach", "Banana");
                     break;
 
                 case 2:
                     GenericsFindMax<int>.FindMaxValue(14, 23, 12);
+                    GenericsFindMax<float>.FindMaxValue(14.4f, 23.5f, 12.9f);
+                    GenericsFindMax<string>.FindMaxValue("Apple","Peach","Banana");
                     break;
+
+                case 3:
+                    new FindMaxGenericsClass<int>(20, 40, 50).TestMaximum();
+                    new FindMaxGenericsClass<float>(20.5f, 10.3f, 11.3f).TestMaximum();
+                    new FindMaxGenericsClass<string>("Apple", "Peach", "Banana").TestMaximum();
+                    break;
+
 
                 default:
                     Console.WriteLine("Please choose the number given range");
